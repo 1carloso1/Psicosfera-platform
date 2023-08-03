897bee73b8a7f2ea4f5b4c83bc1d616a38c6d234
1carloso1 <20200994@uaz.edu.mx>
2023-08-03 11:41:30 -0600

    Se creo la base del proyecto en Django con un tamplate Home de prueba, Django configurado

---
+++ psicosfera/psicosfera/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from home.views import Home
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', Home.as_view(), name = 'home'),
+]
