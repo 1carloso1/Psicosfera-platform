683542c56168a34835a0324e00f33e3cd6471995
1carloso1 <20200994@uaz.edu.mx>
2023-08-05 14:58:44 -0600

    Guardar cambios locales en logo.png

--- psicosfera/login/views.py
+++ psicosfera/login/views.py
@@ -11,6 +11,11 @@ class RegistroView(TemplateView):
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
     
+class RegistroUsuarioView(TemplateView):
+    template_name = 'registro/registroUsuario.html'
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
 class ResContraView(TemplateView):
     template_name = 'registro/reestablecer.html'
     def dispatch(self, request, *args, **kwargs):
