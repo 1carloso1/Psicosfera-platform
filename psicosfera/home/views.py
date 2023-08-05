d42a2ae7950db7af9bbef12e91bd1eea8073ff30
Osmar Ortiz <osmarortiz0004@gmail.com>
2023-08-05 11:54:14 -0600

    Se crea el archivo y base y el template de bienvenida

--- psicosfera/home/views.py
+++ psicosfera/home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
 class Home(TemplateView):
     template_name = 'home.html'
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+class Registro(TemplateView):
+    template_name = 'registro.html'
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
--- psicosfera/psicosfera/urls.py
+++ psicosfera/psicosfera/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from home.views import Home
+from home.views import Home, Registro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Home.as_view(), name = 'home'),
+    path('registro', Registro.as_view(), name = 'registro'),
 ]
