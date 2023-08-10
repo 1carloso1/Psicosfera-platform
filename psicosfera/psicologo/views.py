d821ad443147b62525469efbe10806918fac00c5
Osmar Ortiz <osmarortiz0004@gmail.com>
2023-08-10 00:42:59 -0600

    Se agrega herencia a todos los templates y se les da funcionalidad a los botones de home.html e interfaz-psicologo.html

--- psicosfera/psicologo/views.py
+++ psicosfera/psicologo/views.py
@@ -3,6 +3,6 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class Interfaz(TemplateView):
-    template_name = 'interfaz.html'
+    template_name = 'interfaz-psicologo.html'
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
