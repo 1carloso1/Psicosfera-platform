8722047fb219a1d9b3ab4f4aed89681db55a68bc
Cristian Alvarado <chalaca23ff@gmail.com>
2023-08-14 16:50:57 -0600

    Se termino el fromulario del paciente

--- psicosfera/paciente/models.py
+++ psicosfera/paciente/models.py
@@ -8,15 +8,15 @@ SEXO_CHOICES = (
 # Modelo de paciente
 class Paciente(models.Model):
     nombre = models.CharField(max_length=100)
-    apaterno = models.CharField(max_length=120)
-    amaterno= models.CharField(max_length=120)
-    fecha_nacimiento = models.DateField()
+    apaterno = models.CharField(max_length=120, verbose_name="Primer Apellido")
+    amaterno= models.CharField(max_length=120, verbose_name="Segundo Apellido")
+    fecha_nacimiento = models.DateField(verbose_name="Fecha de Nacimiento")
     edad = models.PositiveIntegerField()
     sexo = models.CharField(max_length=1, choices=SEXO_CHOICES)
-    ocupacion = models.CharField(max_length=100)
-    correo = models.EmailField()
+    ocupacion = models.CharField(max_length=100, verbose_name="Ocupación")
+    correo = models.EmailField(verbose_name="Correo Electrónico")
     telefono = models.CharField(max_length=15)
-    direccion = models.CharField(max_length=200, blank=True)
+    direccion = models.CharField(max_length=200, blank=True, verbose_name="Dirección")
     fecha_registro = models.DateTimeField(auto_now_add=True)
     foto_perfil = models.ImageField(upload_to='pacientes_foto_perfil/', blank=True, null=True)
 
