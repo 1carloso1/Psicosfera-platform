2d8c695de72d07a289a3e7887331dd3110ab8c9c
1carloso1 <20200994@uaz.edu.mx>
2023-08-22 21:21:03 -0600

    Se le dio funcionalidad al home, para psicologos y pacientes

--- psicosfera/home/views.py
+++ psicosfera/home/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class Home(TemplateView):
-    template_name = 'home.html'
+    def get_template_names(self):
+        if self.request.user.groups.filter(name='Psicologos').exists():
+            return ['homePsicologo.html']
+        else:
+            return ['home.html']
+    
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
     
