dec0fa63169c56d3ca0c90b5fca445c4412c4048
1carloso1 <20200994@uaz.edu.mx>
2023-08-24 17:47:24 -0600

    Se esta enlazando el perfil con el modelo de paciente

--- psicosfera/paciente/views.py
+++ psicosfera/paciente/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import ListView, TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
@@ -9,6 +10,8 @@ from .models import Paciente
 from .forms import FormPaciente
 from django.contrib.auth.models import Group
 from django.shortcuts import redirect
+from django.http import HttpResponse, JsonResponse
+import base64
 
 class PerfilView(TemplateView):
     template_name = 'perfil-paciente.html'
@@ -32,6 +35,39 @@ class NuevoPaciente(LoginRequiredMixin, CreateView):
             return super().dispatch(request, *args, **kwargs)
 
 
-    
-      
- 
+
+class DatosPerfilView(View):
+    def get(self, request):
+        paciente_id = request.GET.get('paciente_id')
+        try:
+            paciente = Paciente.objects.get(id=paciente_id)
+            nombre = str(paciente.nombre) + " " + str(paciente.apaterno) + " " + str(paciente.amaterno)
+            correo_electronico = paciente.correo
+            telefono = paciente.telefono
+            direccion = paciente.direccion
+            edad = paciente.edad
+            sexo = paciente.sexo
+            ocupacion = paciente.ocupacion
+            fecha_nacimiento = paciente.fecha_nacimiento
+            if paciente.foto_perfil:
+                with paciente.foto_perfil.open('rb') as image_file:
+                    image_data = image_file.read()
+                    foto = base64.b64encode(image_data).decode('utf-8')
+            else:
+                foto = None
+
+            contexto = {
+                'nombre': nombre,
+                'fecha_nacimiento': fecha_nacimiento,
+                'edad': edad,
+                'sexo': sexo,
+                'ocupacion': ocupacion,
+                'correo_electronico': correo_electronico,
+                'telefono': telefono,
+                'direccion': direccion,
+                'foto': foto,
+            }
+
+            return render(request, 'perfil-paciente.html', contexto)
+        except Paciente.DoesNotExist:
+            return HttpResponse("Paciente no encontrado.", status=404)
