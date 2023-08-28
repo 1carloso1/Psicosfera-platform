98b4a787d704f1af7296143e5c29d92eb2b80b31
Cristian Alvarado <chalaca23ff@gmail.com>
2023-08-27 21:01:34 -0600

    Se actualizo el formulario del psicologo

--- psicosfera/psicologo/models.py
+++ psicosfera/psicologo/models.py
@@ -55,18 +55,19 @@ class Psicologo(models.Model):
     curriculum = models.FileField(upload_to='curriculum_psicologo/', blank=True, null=True)
     foto_perfil = models.ImageField(upload_to='psicologos_foto_perfil/', blank=True, null=True)
     certificado = models.FileField(upload_to='certificados_psicologos/', blank=True, null=True)
+    identificacion_oficial = models.FileField(upload_to='identificacion_psicologos/', blank=True, null=True) 
     enlace_pagina_web = models.URLField(blank=True)
     enlace_facebook = models.URLField(blank=True)
     enlace_instagram = models.URLField(blank=True)
     enlace_linkedin = models.URLField(blank=True)
     fecha_registro = models.DateTimeField(auto_now_add=True)
+    consultorio = models.ForeignKey('Consultorio', verbose_name='Consultorio', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"{self.nombre} - {self.especialidad}"
 
 # Modelo de consultorio de los psicologos
 class Consultorio(models.Model):
-    psicologo = models.ForeignKey('Psicologo', verbose_name="Psicologo", on_delete=models.CASCADE, null=True)
     direccion = models.CharField(max_length=200, blank=True)
     horario_atencion = models.CharField(max_length=200, blank=True)
     servicios_ofrecidos = models.TextField(blank=True)
