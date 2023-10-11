a550388c4d0572c3f38337ecf85da34e0eb69068
1carloso1 <20200994@uaz.edu.mx>
2023-10-11 12:09:14 -0600

    se agregaro la interfaz del paciente

--- psicosfera/psicologo/models.py
+++ psicosfera/psicologo/models.py
@@ -1,19 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from psicologo.especialidades import ESPECIALIDADES_CHOICES_2
-
 
 SEXO_CHOICES = (
     ('M', 'Masculino'),
     ('F', 'Femenino'),
-)
-
-METODOS_PAGO_CHOICES = (
-    ('efectivo', 'Efectivo'),
-    ('tarjeta', 'Tarjeta de Crédito/Débito'),
-    ('transferencia', 'Transferencia Bancaria'),
-    ('paypal', 'PayPal'),
-    # Agrega más métodos de pago según sea necesario
+    ('O', 'Otro'),
 )
 
 
@@ -24,11 +15,10 @@ class Psicologo(models.Model):
     edad = models.PositiveIntegerField()
     sexo = models.CharField(max_length=1, choices=SEXO_CHOICES)
     fecha_nacimiento = models.DateField(null=True, verbose_name="Fecha de nacimiento")
-    especialidad = models.CharField(max_length=100, choices=ESPECIALIDADES_CHOICES_2)
+    especialidad = models.CharField(max_length=200,verbose_name="Especialidad")
     telefono = models.CharField(max_length=10, null=True)
     institucion_otorgamiento = models.CharField(max_length=200, blank=True, null=True, verbose_name="Institución de otorgamiento")
     anio_obtencion = models.PositiveIntegerField(null=True, verbose_name="Año de obtención")
-    metodo_pago = models.CharField(max_length=100, choices=METODOS_PAGO_CHOICES, blank=True, null=True)
     curriculum = models.FileField(upload_to='curriculum_psicologo/', blank=True, null=True)
     foto_perfil = models.ImageField(upload_to='psicologos_foto_perfil/', blank=True, null=True)
     certificado = models.FileField(upload_to='certificados_psicologos/', blank=True, null=True)
@@ -45,11 +35,9 @@ class Psicologo(models.Model):
 # Modelo de consultorio de los psicologos
 class Consultorio(models.Model):
     psicologo = models.ForeignKey('Psicologo', verbose_name='Consultorio', on_delete=models.CASCADE, null=True)
-    direccion = models.CharField(max_length=200, blank=True)
-    horario_atencion = models.CharField(max_length=200, blank=True)
-    servicios_ofrecidos = models.TextField(blank=True)
-
+    direccion = models.CharField(max_length=250,verbose_name="Dirección")
+    horario_atencion = models.TimeField(blank=True, null=True)
     def __str__(self):
-        return f"Consultorio en {self.direccion}"
+        return f"{self.direccion}"
 
     
