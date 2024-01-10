b4f8cc63d5f23009e50ce363df94b63f055367dc
Osmar Ortiz <osmarortiz0004@gmail.com>
2024-01-10 02:16:08 -0600

    Se completa la funcionalidad de notificaciones para psicologo y paciente. Se le agrego css al dropdown de las notificaciones y tambien al icono.

--- psicosfera/home/context_processors.py
+++ psicosfera/home/context_processors.py
@@ -2,7 +2,11 @@ from evento.models import Notification
 
 def notifications(request):
     if request.user.is_authenticated:
-        user_notifications = Notification.objects.filter(user=request.user, is_read=False)
-        return {'user_notifications': user_notifications}
+        notificaciones_no_leidas = 0
+        user_notifications = Notification.objects.filter(user=request.user)
+        for notificacion in user_notifications:
+            if not notificacion.is_read:
+                notificaciones_no_leidas += 1
+        return {'user_notifications': user_notifications, 'notificacionesNoLeidas': notificaciones_no_leidas}
     else:
         return {}
