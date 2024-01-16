66745308046e5f753caece675955d49440c20ba4
1carloso1 <20200994@uaz.edu.mx>
2024-01-16 13:29:53 -0600

    Lista de chats solo muestra a los amigos

--- psicosfera/chat/views.py
+++ psicosfera/chat/views.py
@@ -4,23 +4,71 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model
 from django.views.generic import ListView
+from django.contrib.auth.models import User
+from paciente.models import Paciente
+from psicologo.models import Psicologo
 
-from typing import List
 
-UserModel = get_user_model()
-
+from typing import List, Union
 
 class UsersListView(LoginRequiredMixin, ListView):
     http_method_names = ['get', ]
 
     def get_queryset(self):
-        return UserModel.objects.all().exclude(id=self.request.user.id)
+        user = self.request.user
+        try:
+            psicologo = Psicologo.objects.get(user=user)
+            print("es psicologo")
+            print(psicologo.contactos)
+            if not psicologo.contactos:
+                psicologo.contactos = []
+            
+            return psicologo.contactos
+
+        except Psicologo.DoesNotExist:
+            pass
+        
+        try:
+            paciente = Paciente.objects.get(user=user)
+            print("es paciente")
+            print(paciente.contactos)
+            if not paciente.contactos:
+                paciente.contactos = []
+            
+            return paciente.contactos
+
+        except Paciente.DoesNotExist:
+            pass
+    
+
 
     def render_to_response(self, context, **response_kwargs):
-        users: List[AbstractBaseUser] = context['object_list']
+        # Obtener la lista de IDs de contactos
+        contactos_ids = context['object_list']
+        print(contactos_ids)
 
-        data = [{
-            "username": user.get_username(),
-            "pk": str(user.pk)
-        } for user in users]
+        # Crear una lista de diccionarios con información de cada contacto
+        data = []
+
+        for contacto_id in contactos_ids:
+            try:
+                contacto = Psicologo.objects.get(id=contacto_id)
+                usuario = contacto.user
+            except Psicologo.DoesNotExist:
+                # Si no es Psicologo, intentar obtenerlo como Paciente
+                try:
+                    contacto = Paciente.objects.get(id=contacto_id)
+                    usuario = contacto.user
+                except Paciente.DoesNotExist:
+                    # Manejar el caso en que no se encontró ni Psicologo ni Paciente
+                    print(f"No se encontró ningún usuario con ID {contacto_id}")
+                    continue
+            
+            # Agregar detalles relevantes al diccionario
+            data.append({
+                "username": usuario.username,
+                "pk": str(usuario.pk),
+                # Agrega más campos según sea necesario
+            })
+
         return JsonResponse(data, safe=False, **response_kwargs)
