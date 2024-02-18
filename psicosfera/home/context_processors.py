cb1d1bfcb44f1da4b7b3eb1b9d028572882b0190
1carloso1 <20200994@uaz.edu.mx>
2024-02-18 11:23:40 -0600

    Formato de notificaciones mejorado

--- psicosfera/home/context_processors.py
+++ psicosfera/home/context_processors.py
@@ -3,10 +3,13 @@ from evento.models import Notification
 def notifications(request):
     if request.user.is_authenticated:
         notificaciones_no_leidas = 0
-        user_notifications = Notification.objects.filter(user=request.user)
+        user_notifications = Notification.objects.filter(user=request.user).order_by('-timestamp')
+        notificaciones_leidas = []
         for notificacion in user_notifications:
             if not notificacion.is_read:
                 notificaciones_no_leidas += 1
-        return {'user_notifications': user_notifications, 'notificacionesNoLeidas': notificaciones_no_leidas}
+            else:
+                notificaciones_leidas.append(notificacion)
+        return {'user_notifications': user_notifications, 'notificacionesNoLeidas': notificaciones_no_leidas,'notificacionesLeidas': notificaciones_leidas}
     else:
         return {}
